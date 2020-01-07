4c3d67c7fc279227f213644b2dd0e2347adfbc3e
rightmilk <47874792+rightmilk@users.noreply.github.com>
2020-01-07 23:31:17 +0800

    Update mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+ @2
+    M=0
+    (loop)
+        //if(R0 == 0) goto end;
+        @0
+        D=M
+        @END
+        D;JEQ
+
+        //R2 = R1 + R2
+        @1
+        D=M
+        @2
+        M=D+M
+
+        //R0 = R0 - 1 ;
+        @0
+        M=M-1
+
+        @loop
+        0;JMP
+        (END)
+    (forever)
+        @forever
+        0;JMP
